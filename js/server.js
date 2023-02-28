5bd9ce270bd5c9e95b08f651acb3cc8c6aaea0dc
gilly <nick.gilmore20@gmail.com>
2023-02-28 15:22:17 -0600

    description added

--- js/server.js
+++ js/server.js
@@ -1,38 +1,26 @@
 
-//-- require function helps us grab the package in Node
-//   similar to import or include
-//   The `http` is a built-in package that performs actions on the server
-const http = require('http');
+//-- This pulls in the package from the node_modules directory
+//   This take care of the http, request, and response objects behind the scenes.
+const express = require('express');
+const app = express();
+app.listen(3000, () => {
 
-//-- Create and start a server with the `createServer` from the `http` package
-//    req - request from browser
-//    res - response to give back to browser
-const server = http.createServer((req, res) => {
-    //-- Responds differently based on different URLs
-    if (req.url === '/about'){
-        res.end("The About Page")
-    }else if (req.url === '/contact'){
-        res.end("The Contact Page")
-    }else if (req.url === '/'){
-        res.end('The Home Page')
-    }else {
-        //-- `writeHead` writes the status code of the request
-        res.writeHead(404);
-        res.end('page not found')
-    }
 })
 
+//--    helps us get the specific path to this file
+const path = require('path');
 
-//-- The start of our server to start taking requests
-// -- The port is 3000 for request
-//     A port is a specific gateway on the server to host a particular app.
-server.listen(3000)
+app.get()
 
+app.get('/', (req,res)=>{
+    //--   path.resolve() -> helps us get the full absolute path which otherwise chnages based on different operating systems.
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
 
+app.get('/about', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+})
 
-
-
-
-
-
-
+app.get('/contact', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, 'contact.html'))
+})
