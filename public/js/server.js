c563e696ef4cc80cd1c827f63eee8c9d1a21439b
gilly <nick.gilmore20@gmail.com>
2023-02-28 16:32:53 -0600

    placement was changed

--- public/js/server.js
+++ public/js/server.js
@@ -3,14 +3,15 @@
 //   This take care of the http, request, and response objects behind the scenes.
 const express = require('express');
 const app = express();
-app.listen(3000, () => {
-
-})
-
 //--    helps us get the specific path to this file
 const path = require('path');
-
-app.get()
+//-- including static files like fonts, images and CSS with Express
+//  app.use a function to increase functionality with express by adding a function to our application middleware stack.
+//    `express.static` is a packaged shipped with express that helps us serve static files. any request for assets should get it from public directory.
+app.use(express.static('public'))
+app.listen(3000, () => {
+    console.log("App listening on port 3000")
+})
 
 app.get('/', (req,res)=>{
     //--   path.resolve() -> helps us get the full absolute path which otherwise chnages based on different operating systems.
